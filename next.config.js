e7f43225f2192921a5da70e644d6a4f9d530a007
tokuchii <tokuchii081@gmail.com>
2025-06-11 23:45:47 +0800

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Keep existing devIndicators setting
-  devIndicators: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/kdraken' : '',
 };
 
 module.exports = nextConfig; 
